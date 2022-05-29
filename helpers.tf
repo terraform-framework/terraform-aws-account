a41006f662ea8877bb35930e121f7ca9efb1276b
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2022-05-29 18:04:35 +0100

    scope unknown helper for each account and env

--- helpers.tf
+++ helpers.tf
@@ -10,19 +10,19 @@ locals {
     ]...))) : env => (env == local.current_env_name)
   }
 
-  helper_is_unknown = !anytrue(concat(
-    values(local.helper_is_account),
-    values(local.helper_is_env),
-  ))
-
   helper_is = {
     // Is account helper
-    account = local.helper_is_account
+    account = merge({
+      unknown = !anytrue(values(local.helper_is_account))
+    }, local.helper_is_account)
 
     // Is environment helper
     env = merge({
       // Special helper to always reflect current env as true
       (local.current_env_name) = true
+
+      // Whether environment is unknown
+      unknown = !anytrue(values(local.helper_is_env))
     }, local.helper_is_env)
 
     // Is a build environment
@@ -30,12 +30,5 @@ locals {
 
     // Is a deployment environment
     deployment = (local.current_deployment_name != null)
-
-    // Is either account or env unknown to config
-    unknown = local.helper_is_unknown
   }
-}
-
-output "unknown" {
-  value = local.helper_is_unknown
 }
